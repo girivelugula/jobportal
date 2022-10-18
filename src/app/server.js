b492947e81531a6e0bc8e672b13dabc9cdb1042e
girivelugula <girivelugula.gv@outlook.com>
2022-10-18 14:44:59 +0530

    deploy changes

--- src/app/server.js
+++ src/app/server.js
@@ -1,13 +1,29 @@
-const express = require("express");
- const app  = express();
- const path = require('path');
- const cors  = require("cors");
-app.use(cors());
-app.use(express.static('./dist/client-app'));
-app.get('/*', function (req, res) {
-     res.sendFile(path.join(__dirname, './../dist/client-app/index.html'));
-    console.log('path', path.join(__dirname, './../dist/client-app/index.html'))
+// const express = require("express");
+//  const app  = express();
+//  const path = require('path');
+//  const cors  = require("cors");
+// app.use(cors());
+// app.use(express.static('./dist/client-app'));
+// app.get('/*', function (req, res) {
+//      res.sendFile(path.join(__dirname, './../dist/client-app/index.html'));
+//     console.log('path', path.join(__dirname, './../dist/client-app/index.html'))
+// });
+//  // Start the app by listening on the default
+//  // Heroku port
+//  app.listen(process.env.PORT || 8080);
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/client-app'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/client-app/index.html'));
 });
- // Start the app by listening on the default
- // Heroku port
-app.listen(process.em.vom || 8080);
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
